493a99f0e7e95b7c9d943117513171e6d169b0c1
Gregory Alves <gregoryalves@hotmail.com>
2019-11-07 18:49:03 -0300

    Removido suporte ao wav.

--- content/js/uploadRules.js
+++ content/js/uploadRules.js
@@ -3,13 +3,13 @@ function carregarRegras(documentBody){
 
     if (blob != null) {
         let extensao = blob.name.substring(blob.name.indexOf(".") + 1);
-        
+
         documentBody.getElementById("titulouploadarea").innerHTML = blob.name;
 
-        if ((extensao == "mp3") || (extensao == "mp4") || (extensao == "flac") || (extensao == "wav")){            
+        if ((extensao == "mp3") || (extensao == "mp4") || (extensao == "flac")){            
             $("#btnSubmit").prop("disabled", false);
         } else {
-            ohSnap('Tipo de arquivo inválido! Tipos suportados: MP3, MP4, FLAC e WAV.', {color: 'red'});
+            ohSnap('Tipo de arquivo inválido! Tipos suportados: MP3, MP4 e FLAC.', {color: 'red'});
             $("#btnSubmit").prop("disabled", true);
         }
                 
