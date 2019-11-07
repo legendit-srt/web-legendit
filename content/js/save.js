7cb772b7a42a5868d2a2768323d8adc7418c281b
Gregory Alves <gregoryalves@hotmail.com>
2019-11-06 23:50:03 -0300

    Capada fora nos comentarios dos JS que o vitao implementou do zero.

--- content/js/save.js
+++ content/js/save.js
@@ -11,8 +11,6 @@ function bom (blob, opts) {
     opts = { autoBom: !opts }
   }
 
-  // prepend BOM for UTF-8 XML and text/* types (including HTML)
-  // note: your browser will automatically convert UTF-16 U+FEFF to EF BB BF
   if (opts.autoBom && /^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(blob.type)) {
     return new Blob([String.fromCharCode(0xFEFF), blob], { type: blob.type })
   }
@@ -34,7 +32,6 @@ function download (url, name, opts) {
 
 function corsEnabled (url) {
   var xhr = new XMLHttpRequest()
-  // use sync to avoid popup blocker
   xhr.open('HEAD', url, false)
   try {
     xhr.send()
@@ -42,7 +39,6 @@ function corsEnabled (url) {
   return xhr.status >= 200 && xhr.status <= 299
 }
 
-// `a.click()` doesn't work for all browsers (#465)
 function click (node) {
   try {
     node.dispatchEvent(new MouseEvent('click'))
@@ -55,11 +51,9 @@ function click (node) {
 }
 
 var saveAs = _global.saveAs || (
-  // probably in some web worker
   (typeof window !== 'object' || window !== _global)
     ? function saveAs () { /* noop */ }
 
-  // Use download attribute first if possible (#193 Lumia mobile)
   : 'download' in HTMLAnchorElement.prototype
   ? function saveAs (blob, name, opts) {
     var URL = _global.URL || _global.webkitURL
@@ -67,13 +61,9 @@ var saveAs = _global.saveAs || (
     name = name || blob.name || 'download'
 
     a.download = name
-    a.rel = 'noopener' // tabnabbing
-
-    // TODO: detect chrome extensions & packaged apps
-    // a.target = '_blank'
+    a.rel = 'noopener' 
 
     if (typeof blob === 'string') {
-      // Support regular links
       a.href = blob
       if (a.origin !== location.origin) {
         corsEnabled(a.href)
@@ -83,14 +73,12 @@ var saveAs = _global.saveAs || (
         click(a)
       }
     } else {
-      // Support blobs
       a.href = URL.createObjectURL(blob)
-      setTimeout(function () { URL.revokeObjectURL(a.href) }, 4E4) // 40s
+      setTimeout(function () { URL.revokeObjectURL(a.href) }, 4E4)
       setTimeout(function () { click(a) }, 0)
     }
   }
 
-  // Use msSaveOrOpenBlob as a second approach
   : 'msSaveOrOpenBlob' in navigator
   ? function saveAs (blob, name, opts) {
     name = name || blob.name || 'download'
@@ -109,10 +97,7 @@ var saveAs = _global.saveAs || (
     }
   }
 
-  // Fallback to using FileReader and a popup
   : function saveAs (blob, name, opts, popup) {
-    // Open a popup immediately do go around popup blocker
-    // Mostly only available on user interaction and the fileReader is async so...
     popup = popup || open('', '_blank')
     if (popup) {
       popup.document.title =
@@ -126,14 +111,13 @@ var saveAs = _global.saveAs || (
     var isChromeIOS = /CriOS\/[\d]+/.test(navigator.userAgent)
 
     if ((isChromeIOS || (force && isSafari)) && typeof FileReader !== 'undefined') {
-      // Safari doesn't allow downloading of blob URLs
       var reader = new FileReader()
       reader.onloadend = function () {
         var url = reader.result
         url = isChromeIOS ? url : url.replace(/^data:[^;]*;/, 'data:attachment/file;')
         if (popup) popup.location.href = url
         else location = url
-        popup = null // reverse-tabnabbing #460
+        popup = null 
       }
       reader.readAsDataURL(blob)
     } else {
@@ -141,8 +125,8 @@ var saveAs = _global.saveAs || (
       var url = URL.createObjectURL(blob)
       if (popup) popup.location = url
       else location.href = url
-      popup = null // reverse-tabnabbing #460
-      setTimeout(function () { URL.revokeObjectURL(url) }, 4E4) // 40s
+      popup = null 
+      setTimeout(function () { URL.revokeObjectURL(url) }, 4E4) 
     }
   }
 )
